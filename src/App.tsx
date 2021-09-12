5decae1b7a938fad0a3898c463b0c43f8abee21a
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-09-13 04:22:11 +0530

    commit: moved project to root

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {Quiz} from "./Pages/Quiz/Quiz"
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <main>
         <Routes>
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./footer.style.css"
 
 export const Footer = () => {
